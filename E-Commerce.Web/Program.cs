f759e96c58afca310921b5ba5f640ea5b81206f5
Mohamed Ramdan <mohamedramdan10.2001@gmail.com>
2025-04-15 06:19:47 +0200

    Add project files.

---
+++ E-Commerce.Web/Program.cs
@@ -0,0 +1,37 @@
+
+namespace E_Commerce.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Add services to the container
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer(); // For Swagger
+            builder.Services.AddSwaggerGen();// For Swagger
+            #endregion
+
+            var app = builder.Build();
+
+            #region Configure the HTTP request pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers(); 
+            #endregion
+
+            app.Run();
+        }
+    }
+}
