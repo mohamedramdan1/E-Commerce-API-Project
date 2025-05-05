40933285643bea7f3aa07ac8add743641fbd6532
Mohamed Ramdan <mohamedramdan10.2001@gmail.com>
2025-05-06 01:55:33 +0300

    05 - Order profile [Mapping]

--- Core/Service/MappingProfiles/OrderProfile.cs
+++ Core/Service/MappingProfiles/OrderProfile.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using AutoMapper;
 using DomainLayer.Models.OrderModule;
 using Shared.DataTransferObjects.IdentityDtos;
+using Shared.DataTransferObjects.OrderDTos;
 
 namespace Service.MappingProfiles
 {
@@ -13,7 +14,15 @@ namespace Service.MappingProfiles
     {
         public OrderProfile()
         {
-            CreateMap<AddressDTo, OrderAddress>();
+            CreateMap<AddressDTo, OrderAddress>().ReverseMap();
+
+            CreateMap<Order, OrderToReturnDTo>()
+                .ForMember(dist => dist.DeliveryMethod, Option => Option.MapFrom(Src => Src.DeliveryMethod.ShortName));
+
+            CreateMap<OrderItem, OrderItemDTo>()
+                .ForMember(dist => dist.ProductName, Option => Option.MapFrom(Src => Src.Product.ProductName))
+                .ForMember(dist => dist.PictureUrl, Option => Option.MapFrom<OrderItemPictureUrlResolver>());
+                
 
         }
     }
