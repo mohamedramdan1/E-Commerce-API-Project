01abf5a4d4d3085f9bb246f41c14c75216c6820a
Mohamed Ramdan <mohamedramdan10.2001@gmail.com>
2025-06-13 01:49:26 +0300

    download stripe.net in Service Package

--- Core/ServiceAbstraction/IPaymentService.cs
+++ Core/ServiceAbstraction/IPaymentService.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shared.DataTransferObjects.BsketModuleDTos;
 
 namespace ServiceAbstraction
 {
     public interface IPaymentService
     {
-        //TODO
+        Task<BasketDTo> CreateOrUpdatePaymentIntentAsync(string BasketId);
+
     }
 }
